44af539ca33c776ee50690b0e3643bc5e22da55d
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-05-31 21:24:43 +0300

    modul 1

---
+++ modul1/part1.py
@@ -0,0 +1,23 @@
+# print('Hello World', 'My Object', end='\n', sep=' - abcd - ')
+#
+# print('print')
+#
+# print('3')
+# print(type(3))
+
+print(3 + 3)
+print("3" + "3")
+print(3 * 3)
+print("3" * 5)
+#print("3" * "3")
+
+######################################
+#                                    #
+######################################
+
+my_value = "My Object"
+# my_value = 15
+my_value2 = 10
+
+print(my_value * my_value2)
+
