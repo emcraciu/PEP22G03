bfe1cd5350c9b0a65ddaf2ad61e96bf5aaa9e963
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-06-14 19:10:29 +0300

    modul 3

---
+++ modul3/part1.py
@@ -0,0 +1,42 @@
+# my_age = 14
+#
+# if my_age >= 18:
+#     print('You are an adult')
+# elif my_age >= 13:
+#     print('you are a teenager')
+# elif my_age >= 0:
+#     print('you are a child')
+# else:
+#     print('Negative values are nto allowed')
+#
+# print('dONE')
+
+# # AND WITH CONDITION
+#
+# a = ""
+# b = False
+#
+# if a:
+#     print(b)
+# else:
+#     print(a)
+#
+# print(a and b)
+#
+# # OR WITH CONDITION
+#
+# if a:
+#     print(a)
+# else:
+#     print(b)
+#
+# print(a or b)
+
+
+# condition
+a = ""
+b = "b"
+
+print(a and b)
+print(bool(a and b))
+print(bool(a))
