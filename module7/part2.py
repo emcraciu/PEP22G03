6f35bf67ddf731752d28b58604a6518e6a17bd39
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-07-19 20:29:39 +0300

    modul 7

---
+++ module7/part2.py
@@ -0,0 +1,25 @@
+# class for store
+
+# - open shop with some products
+# - add products
+# - remove products
+
+class Shop:
+
+    def __init__(self, products):
+        self.products = products
+
+    def add_product(self, name, quantity):
+        self.products[name] = quantity
+
+    def remove_product(self, name):
+        del self.products[name]
+
+my_shop = Shop({'camera': 5, 'mouse': 2})
+print(my_shop.products)
+my_shop.add_product('keyboard', 3)
+print(my_shop.products)
+my_shop.remove_product('mouse')
+print(my_shop.products)
+my_shop.add_product('keyboard', 2)
+print(my_shop.products)
