ebd614a0e51f69c9ef6dfbd7622bcaea2f5acb9c
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-07-26 19:32:56 +0300

    modul 7

--- module7/part4.py
+++ module7/part4.py
@@ -39,3 +39,70 @@
 # new_car = New_Car(200)
 # new_car.hp = Car.calculate_hp(new_car.hp)
 # print(new_car.hp)
+
+
+# class Car():
+#     turbo = False
+#     wheels = 0
+#
+#     def __init__(self, hp: int):
+#         self.hp = hp
+#         self.add_wheels()
+#
+#     def add_wheels(self):
+#         self.wheels += 4
+#
+#     def calculate_hp(self):
+#         if self.hp <= 50:
+#             self.hp = self.hp + (10 * self.hp) / 100
+#         else:
+#             self.hp = self.hp + (5 * self.hp) / 100
+#
+#     def add_turbo(self):
+#         self.turbo = True
+#         self.calculate_hp()
+#
+#
+# class Truck(Car):
+#
+#     def add_wheels(self):
+#         self.wheels += 6
+#
+#     def add_turbo(self):
+#         self.turbo = 2
+#         self.calculate_hp()
+#
+#
+# car = Car(45)
+# print(dir(car))
+# print("Car wheels", car.wheels)
+# car.add_turbo()
+# print("Car HP", car.hp)
+#
+# truck = Truck(100)
+# print(dir(truck))
+# print("Truck wheels", truck.wheels)
+# truck.add_turbo()
+# print("Truck HP", truck.hp)
+
+
+
+class A():
+    def abc(self):
+        print('abc')
+
+class B(A):
+    def abc(self):
+        print('bac')
+
+class C(B, A):
+    pass
+
+a = A()
+a.abc()
+
+b = B()
+b.abc()
+
+c = C()
+c.abc()
