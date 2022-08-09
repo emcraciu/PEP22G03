edec016ae45caee454b1ec42641727cd61001fc1
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-08-09 21:20:08 +0300

    m8

--- module8/app3.py
+++ module8/app3.py
@@ -1,13 +1,32 @@
-# write python code to file
-
 class PythonWriter:
 
     def __init__(self):
         self.file = open("my_script.py", "w")
 
     def get_input(self):
+        var1 = ">>>"
+        var2 = "..."
+        var3 = "    "
+        counter = 0
+        in_try = False
         while True:
-            input(">>>")
+            x = input(f"{var1}")
+            self.file.write(counter * "    " + x + "\n")
+            try:
+                if x[-1] == ":":
+                    counter += 1
+                    var1 = "..." + counter * var3
+                if x.endswith("try:"):
+                    in_try = True
+                if x.endswith("finally:"):
+                    in_try = False
+            except IndexError:
+                if counter == 0 and not in_try:
+                    self.file.close()
+                    return
+                counter -= 1
+                var1 = "..." + counter * var3
+
 
 py = PythonWriter()
 py.get_input()
