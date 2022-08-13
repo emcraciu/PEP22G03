7124e33ded61f37baf5588a0813b13c768ba0c83
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-08-13 16:24:41 +0300

    exam

---
+++ exam2/shop_app.py
@@ -0,0 +1,25 @@
+"""
+A shop needs an iterable object to keep track of product expiration dates.
+Each product in the shop will have a string name, datetime expiration
+Iterating the object will return all products in the shop ordered by expiration date
+1) 40p: Definition
+    a) 10p: Basic class structure of objects
+    b) 10p: Basic class structure of iterator
+    c) 10p: Method to add product to the shop
+    d) 10p: Method to remove product that expired
+2) 40p: Execution:
+    a) 10p: Create instance of your shop
+    b) 10p: Add products:
+        - Banana, 11 Aug 2022
+        - Orange, 12 Aug 2022
+        - Kiwi, 13 Aug 2022
+        - Apple, 14 Aug 2022
+        - Peach, 15 Aug 2022
+    c) 10p: Remove expired products
+    d) 10p: Iterate the created object writing each product on a new line in file in the order of expiration
+3) 20p: Documenting:
+   a) 5p: type hints for arguments
+   b) 5p: module documentation
+   c) 5p: class documentation for all classes
+   d) 5p: method documentation for all methods
+"""
